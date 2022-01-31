cecfcb12857661a5e3b3e56c1d6a09ad3d22c24b
Sergio <sergonji@DESKTOP-DVFB9RJ.localdomain>
2022-01-31 23:58:16 +0100

    Update

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ pasa unos test
 
 despliega
 
--------
+----------------
 
 
 docker network create app
@@ -17,7 +17,11 @@ docker-compose -f ./app/node_project/docker-compose.app.yml up -d
 
 docker-compose -f ./app/jenkins/docker-compose.jenkins.yml up -d
 
---------
+----------------
+
 docker-compose -f ./app/mysql/docker-compose.mysql.yml down ;
 docker-compose -f ./app/node_project/docker-compose.app.yml down ;
 docker-compose -f ./app/jenkins/docker-compose.jenkins.yml down
+
+
+----------------
