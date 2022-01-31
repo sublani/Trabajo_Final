b598a0285df1a849ad0fb2f835d7c9659fd1cad3
Sergio <sergonji@DESKTOP-DVFB9RJ.localdomain>
2022-01-31 23:55:54 +0100

    Update

--- start.sh
+++ start.sh
@@ -1,1 +1,23 @@
+JEnkinsfile
+
+docker build -t app:0.1.0 .
+
+pasa unos test
+
+despliega
+
+-------
+
+
 docker network create app
+
+docker-compose -f ./app/mysql/docker-compose.mysql.yml up -d
+
+docker-compose -f ./app/node_project/docker-compose.app.yml up -d
+
+docker-compose -f ./app/jenkins/docker-compose.jenkins.yml up -d
+
+--------
+docker-compose -f ./app/mysql/docker-compose.mysql.yml down ;
+docker-compose -f ./app/node_project/docker-compose.app.yml down ;
+docker-compose -f ./app/jenkins/docker-compose.jenkins.yml down
