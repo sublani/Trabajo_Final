df308b1fec6df6360305ef971a00e5cc13055de0
sergonji <sergonji@gmail.com>
2022-02-04 21:12:28 +0000

    Update

--- start.sh
+++ start.sh
@@ -1,27 +1,29 @@
-JEnkinsfile
+#JEnkinsfile
 
-docker build -t app:0.1.0 .
+#docker build -t app:0.1.0 .
 
-pasa unos test
+#pasa unos test
 
-despliega
+#despliega
 
-----------------
+#----------------
 
+cp /opt/Trabajo_Final/app/node_project/* /opt/gitlab/app/ -R
 
-docker network create app
+docker network create app ;
 
-docker-compose -f ./app/mysql/docker-compose.mysql.yml up -d
+docker-compose -f ./app/mysql/docker-compose.mysql.yml up -d ;
 
-docker-compose -f ./app/node_project/docker-compose.app.yml up -d
+docker-compose -f ./app/node_project/docker-compose.app.yml up -d ;
 
-docker-compose -f ./app/jenkins/docker-compose.jenkins.yml up -d
+docker-compose -f ./app/jenkins/docker-compose.jenkins.yml up -d ;
 
-----------------
+nohup /opt/Trabajo_Final/app/jenkins-slave/start_jenkins_slave.sh &
+
+#----------------
 
 docker-compose -f ./app/mysql/docker-compose.mysql.yml down ;
 docker-compose -f ./app/node_project/docker-compose.app.yml down ;
-docker-compose -f ./app/jenkins/docker-compose.jenkins.yml down
+docker-compose -f ./app/jenkins/docker-compose.jenkins.yml down ;
 
-
-----------------
+#----------------
